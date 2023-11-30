bd3286e8c1b6b77cc3ee70a58c65d5ae5310407f
MuneebCH002 <muneebch309@gmail.com>
2023-11-30 22:05:42 +0500

    dodge

---
+++ Assets/Scripts/ObjectHit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    MeshRenderer mr;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        { 
+        mr = GetComponent<MeshRenderer>();
+        mr.material.color= Color.red;
+            gameObject.tag = "hits";
+        }
+    }
+}
---
+++ Assets/Scripts/mover.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour
+{
+    [SerializeField] float movespeed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    printinstruction();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Moveplayer();
+
+    }
+    void printinstruction()
+    {
+       // Debug.Log("Start game");
+        //Debug.Log("Control player by arroe key");
+    }
+    void Moveplayer()
+    {
+        float xvalue = Input.GetAxis("Horizontal") * Time.deltaTime * movespeed;
+        float zvalue = Input.GetAxis("Vertical") * Time.deltaTime * movespeed;
+        transform.Translate(xvalue, 0, zvalue);
+    }
+}
+   
